adce0da6e10fa014b7a616d0732b4f202ebcd90f
mjxw <matthewjwu@gmail.com>
2016-12-01 19:28:37 -0800

    Finished login, imported template for dashboard

--- app.js
+++ app.js
@@ -35,9 +35,10 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 //Set static path
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static(path.join(__dirname, 'views')));
-app.use('/js', express.static(path.join(__dirname + '/views/')));
-app.use('/css', express.static(path.join(__dirname + '/views/')));
+app.use('/css', express.static(path.join(__dirname + '/public/css')));
+app.use('/js', express.static(path.join(__dirname + '/public/js')));
+app.use('/fonts', express.static(path.join(__dirname + '/public/fonts')));
+app.use('/tables', express.static(path.join(__dirname + '/public/tables')));
 
 // Handling routes
 app.get('/', function(req, res){
@@ -45,18 +46,26 @@ app.get('/', function(req, res){
 });
 
 app.get('/clientDashboard', function(req, res){
-  res.render('clientDash.ejs');
+  res.render('template_index.ejs');
+});
+
+app.get('/userDashboard', function(req, res){
+  res.render('userDash.ejs');
+});
+
+app.get('/trainerDashboard', function(req, res){
+  res.render('trainerDash.ejs');
 });
 
 // Login page
 app.post('/login', function(req, res){
   var email = req.body.email;
   var pass = req.body.pass;
-  console.log("form submitted");
+  console.log("login form submitted");
   console.log("user entered: " + email);
   console.log("user entered: " + pass);
 
-  connection.query("SELECT * from Accounts WHERE Accounts.email = " + "'" + email + "'" + " AND Accounts.password = " + "'" + pass +"'", function(err, rows, fields) {
+  connection.query("SELECT * FROM Accounts WHERE Accounts.email = " + "'" + email + "'" + " AND Accounts.password = " + "'" + pass +"'", function(err, rows, fields) {
     // console.log(fields);
     // console.log(err);
     if (err) {
@@ -69,13 +78,50 @@ app.post('/login', function(req, res){
     // If user is found
     if (rows.length) {
       console.log('successful login');
-      res.redirect('/clientDashboard');
+      console.log(rows[0].type);
+      if (rows[0].type == 'user') {
+        res.redirect('/userDashboard');
+      } else if (rows[0].type == 'client') {
+        res.redirect('/clientDashboard');
+      } else if (rows[0].type == 'trainer') {
+        res.redirect('/trainerDashboard');
+      }
     } else {
       console.log('failed login');
       res.send('The email and or password you submitted was not found');
     }
-    // connection.end();
   });
+});
+
+// Registration
+app.post('/register', function(req, res){
+  var email = req.body.email;
+  var pass = req.body.pass;
+  var type = req.body.type;
+  console.log("registration form submitted")
+  console.log("user entered " + email);
+  console.log("user entered " + pass);
+  console.log("user entered " + type);
+
+
+  var query = "INSERT INTO Accounts(type, email, password) " +
+              "VALUES (" + "'" + type + "', " + "'" + email + "', " + "'" + pass + "')";
+
+
+  connection.query(query, function (err, rows, fields){
+
+
+    if (err) {
+      console.log(err);
+      res.send('There is something wrong with your registration, please email us at matthewjwu@gmail.com');
+    } else {
+      console.log(rows);
+      console.log('successful registration');
+      res.redirect('/');
+    }
+  });
+
+
 });
 
 app.listen(5580, function(){
